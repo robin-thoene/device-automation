6964e038f08b5f9e1e6615a6653243d0cdb57385
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2023-05-10 20:20:31 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # device-automation
+
 Collection of automation code for my machines.
+
+## MacOS
+
+1. Open a terminal
+2. Execute `curl -fsSL https://raw.githubusercontent.com/robin-thoene/device-automation/main/macos/install.sh > install.sh && chmod +x install.sh && ./install.sh`
+3. Follow the instructions
---
+++ macos/install.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+#################
+### Initial setup
+#################
+
+# Ask for sudo upfront.
+echo "Some operations need sudo privileges. Please enter your password."
+sudo -v
+# Ask user for required information.
+read -p "Enter your work company name: " company_name
+# Define needed variables.
+git_user_dir=~/dev/robin-thoene
+git_company_dir=~/dev/$company_name
+git_dracula_dir=~/dev/dracula
+# Create the directory to store git repos.
+mkdir -p $git_user_dir
+mkdir -p $git_company_dir
+mkdir -p $git_dracula_dir
+
+###################################
+### Apply operating system settings
+###################################
+
+echo "Setting up operating system settings..."
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
+defaults write com.apple.dock tilesize -integer 20
+killall Dock
+echo "Done."
+
+###########################
+### Install package manager
+###########################
+
+echo "Installing brew..."
+xcode-select --install
+which -s brew
+if [[ $? != 0 ]]; then
+    yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/Users/$USER/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    brew update
+fi
+echo "Done."
+
+##############################
+### Install blocking packages.
+##############################
+
+echo "Installing blocking packages please stay here and enter credentials as needed..."
+brew install --cask dotnet-sdk
+brew install --cask microsoft-teams
+brew install --cask mactex
+brew install --cask microsoft-word
+echo "Done. You can now go and let the script install the rest."
+
+################
+### Install git.
+################
+
+echo "Installing git..."
+brew install git
+echo "Done."
+
+###############
+### Install ZSH
+###############
+
+echo "Installing Zsh and Oh My Zsh..."
+# Zsh
+if ! brew list zsh &>/dev/null; then
+    # Install Zsh.
+    brew install zsh
+    # Install Oh My Zsh.
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    # Install the dracula theme.
+    cd $git_dracula_dir && git clone https://github.com/dracula/zsh.git
+    ln -s $git_dracula_dir/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+    # Activate the dracula theme.
+    sed -i '' 's/robbyrussell/dracula/g' ~/.zshrc
+fi
+echo "Done."
+
+###########
+### Browser
+###########
+
+echo "Installing browsers..."
+brew install --cask firefox
+brew install --cask google-chrome
+echo "Done."
+
+###########
+### Utility
+###########
+
+echo "Installing utility packages..."
+brew install --cask macpass
+brew install --cask inkscape
+brew install --cask teamviewer
+brew tap homebrew/cask-drivers
+brew install logitech-options
+brew install --cask gyazo
+brew install --cask iterm2
+# Download the dracula theme for iterm2.
+cd $git_dracula_dir && git clone https://github.com/dracula/iterm.git
+brew install --cask nordvpn
+brew install --cask alfred
+brew install --cask logseq
+echo "Done."
+
+###############
+### Development
+###############
+
+echo "Installing development packages..."
+# CLI
+brew install --cask powershell
+brew install nvm
+# Add nvm path to shells.
+echo "export NVM_DIR="$HOME/.nvm"
+. "$(brew --prefix nvm)/nvm.sh"" >>~/.zshrc
+# Reload shells.
+source ~/.zshrc
+# Install latest LTS version of node.
+nvm install --lts
+brew install yarn
+brew install podman
+brew install azure-cli
+brew install nuget
+brew tap azure/bicep
+brew install bicep
+brew install rbenv ruby-build
+brew install cocoapods
+
+# SDK
+dotnet tool install --global dotnet-ef
+brew install openjdk
+
+# IDE / Editor
+brew install --cask visual-studio-code
+brew install --cask rider
+
+# Tools
+brew install --cask microsoft-azure-storage-explorer
+brew install --cask postman
+brew install --cask azure-data-studio
+brew install --cask mysqlworkbench
+brew install --cask redisinsight
+echo "Done."
+
+#################
+### Communication
+#################
+
+echo "Installing communication packages..."
+brew install --cask thunderbird
+brew install --cask discord
+brew install --cask signal
+echo "Done."
+
+#########
+### Media
+#########
+
+echo "Installing media packages..."
+brew install --cask spotify
+brew install --cask vlc
+echo "Done."
+
+################
+### Office Tools
+################
+
+echo "Installing office tools..."
+brew install --cask texmaker
+brew install --cask microsoft-excel
+brew install --cask microsoft-powerpoint
+brew install --cask onedrive
+echo "Done."
+
+###########
+# App Store
+###########
+
+read -p "Sign in to App Store and press ENTER to continue..."
+
+echo "Installing App Store packages..."
+# Install command line tool for app store
+brew install mas
+
+# Install XCode from app store.
+mas install 497799835
+# Install ColorSlurp from app store.
+mas install 1287239339
+# Install better snap tool from app store.
+mas install 417375580
+# Install Outlook from app store.
+mas install 985367838
+echo "Done."
+
+read -p "Please start iterm2 and set the dracula color theme."
+read -p "Please set iterm2 as default terminal."
