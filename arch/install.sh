c29562336cb6068dadf7e4462107766c108bce96
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-05-02 22:23:37 +0200

    define sections for post install script

---
+++ arch/install.sh
@@ -0,0 +1,224 @@
+#!/bin/sh
+
+# This script installs and configures Arch Linux after the initial setup process
+# and is split into different sections, following this general post installation 
+# recommendations that are relevant for this setup
+# https://wiki.archlinux.org/title/General_recommendations
+
+# System administration
+
+## Users and groups
+
+### TODO
+
+## Security
+
+### TODO
+
+## Service management
+
+### TODO
+
+## System maintenance
+
+# Package management
+
+## pacman
+
+### TODO
+
+## Repositories
+
+### TODO
+
+## Mirrors
+
+### TODO
+
+## Arch Build System
+
+### TODO
+
+## Arch User Repository
+
+### TODO
+
+# Booting
+
+## Hardware auto-recognition
+
+### TODO
+
+## Microcode
+
+### TODO
+
+## Retaining boot messages
+
+### TODO
+
+## Num Lock activation
+
+### TODO
+
+# Graphical user interface
+
+## Display server
+
+### TODO
+
+## Display drivers
+
+### TODO
+
+## Desktop environments
+
+### TODO
+
+## Window managers or compositors
+
+### TODO
+
+## Display manager
+
+### TODO
+
+## User directories
+
+### TODO
+
+# Power management
+
+## ACPI events
+
+### TODO
+
+## CPU frequency scaling
+
+### TODO
+
+## Laptops
+
+### TODO
+
+## Suspend and hibernate
+
+### TODO
+
+# Multimedia
+
+## Sound system
+
+### TODO
+
+# Networking
+
+## DNS security
+
+### TODO
+
+## Setting up a firewall
+
+### TODO
+
+## Network shares
+
+### TODO
+
+# Input devices
+
+## Keyboard layouts
+
+### TODO
+
+## Mouse buttons
+
+### TODO
+
+## Laptop touchpads
+
+### TODO
+
+## TrackPoints
+
+### TODO
+
+# Optimization
+
+## Benchmarking
+
+### TODO
+
+## Improving performance
+
+### TODO
+
+## Solid state drives
+
+### TODO
+
+# System services
+
+## File index and search
+
+### TODO
+
+## Local mail delivery
+
+### TODO
+
+## Printing
+
+### TODO
+
+# Appearance
+
+## Fonts
+
+### TODO
+
+## GTK and Qt themes
+
+### TODO
+
+# Console improvements
+
+## Tab-completion enhancements
+
+### TODO
+
+## Aliases
+
+### TODO
+
+## Alternative shells
+
+### TODO
+
+## Bash additions
+
+### TODO
+
+## Colored output
+
+### TODO
+
+## Compressed files
+
+### TODO
+
+## Console prompt
+
+### TODO
+
+## Emacs shell
+
+### TODO
+
+## Mouse support
+
+### TODO
+
+## Session management
+
+### TODO
